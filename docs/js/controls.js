ee1794252f511541ceec47b1d5ce7a96199774d3
AdrianChC <adrianchuquipiondo@gmail.com>
2026-07-28 16:35:57 -0500

    updated responsive design

--- docs/js/controls.js
+++ docs/js/controls.js
@@ -1,31 +1,67 @@
 
+
+// References to controls
+let navControl = null;
+let fullscreenControl = null;
+let geolocateControl = null;
+
+
+// Adds/removes controls depending on screen width
+function updateControls(map) {
+
+    // Remove all existing top-right controls
+    if (navControl) {
+        map.removeControl(navControl);
+        navControl = null;
+    }
+
+    if (fullscreenControl) {
+        map.removeControl(fullscreenControl);
+        fullscreenControl = null;
+    }
+
+    if (geolocateControl) {
+        map.removeControl(geolocateControl);
+        geolocateControl = null;
+    }
+
+    if (window.innerWidth > 600) {
+
+        navControl = new maplibregl.NavigationControl();
+
+        fullscreenControl = new maplibregl.FullscreenControl({
+            container: document.getElementById("container")
+        });
+
+        geolocateControl = new maplibregl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        });
+
+        map.addControl(navControl, "top-right");
+        map.addControl(fullscreenControl, "top-right");
+        map.addControl(geolocateControl, "top-right");
+
+    } else {
+
+        geolocateControl = new maplibregl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+        });
+
+        map.addControl(geolocateControl, "top-right");
+
+    }
+
+}
+
+
 // Navigation controls
 export function addControls(map){
-
-    // Navigation control
-    map.addControl(
-        new maplibregl.NavigationControl(),
-        "top-right"
-    );
-
-    // Full Screen control
-    map.addControl(
-        new maplibregl.FullscreenControl({
-            container: document.getElementById("container")
-        }),
-        "top-right"
-    );
-
-    // Geolocate
-    map.addControl(
-        new maplibregl.GeolocateControl({
-            positionOptions:{
-                enableHighAccuracy:true
-            },
-            trackUserLocation:true
-        }),
-        "top-right"
-    );
 
     // Scale bar
     map.addControl(
@@ -35,6 +71,10 @@ export function addControls(map){
         }),
         "bottom-left"
     );
+
+    updateControls(map);
+
+    window.addEventListener("resize", () => updateControls(map));
 
 }
 
